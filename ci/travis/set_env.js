d6fa4bbcb3021ca9524c1c977c80373c7c1f9552
Taka Okunishi <okunishinish@gmail.com>
2017-05-08 12:29:57 +0900

    OSS対応

--- ci/travis/set_env.js
+++ ci/travis/set_env.js
@@ -10,7 +10,5 @@ const { setEnv } = require('sugos-travis')
 const { PublicRepo, PrivateRepo } = setEnv.presets
 
 setEnv({
-  // TODO OSS
-  // values: PublicRepo,
-  values: PrivateRepo
+  values: PublicRepo,
 })
